b1c4eed6407b63b1f85248ecd2f32996203c7e02
sahmnlama <sahan.lamahewa@gmail.com>
2018-03-16 11:34:32 +0530

    first commit

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,46 +1,38 @@
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+
+// device APIs are available
+//
+function onDeviceReady() {
+document.getElementById("audioCapture").addEventListener("click", audioCapture);
+}
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+      */
+      function mediaSuccess(){
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+      };
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+      function audioCapture() {
+        alert('dsfsdf1');
+        alert(navigator.device.capture);
+   var options = {
+      limit: 1,
+      duration: 10
+   };
+   navigator.device.capture.captureAudio(onSuccess, onError, options);
+alert('dsfsdf');
+   function onSuccess(mediaFiles) {
+      var i, path, len;
+      for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+         path = mediaFiles[i].fullPath;
+         console.log(mediaFiles);
+         alert(mediaFiles);
+      }
+   }
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
+   function onError(error) {
+      alert('Error code: ' + error.code, null, 'Capture Error');
+   }
+}
